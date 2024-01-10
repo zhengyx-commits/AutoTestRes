6a3b0ffbb911dcbd754a770d621c5dcbd68f1865
yongbo.shao <yongbo.shao@amlogic.com>
2024-01-10 20:20:53 +0800

    AutoTestRes: Add res. [1/1]
    PD#AUT-1494
    
    Problem:
    sky need test resources.
    
    Solution:
    Add res.
    
    Verify:
    s905x4
    
    Signed-off-by: yongbo.shao <yongbo.shao@amlogic.com>
    Change-Id: Ia7c173f6c5d819e2eee2928f558e4a2f6cb188a6

---
+++ scripts/python/res/debug/dump_kernel.sh
@@ -0,0 +1,20 @@
+<pre>
+<a href="DVBKPI/">DVBKPI/</a>
+<a href="H264_Butterfly_4k_High@L5.1_AAC_30M_30fps_3min.ts">H264_Butterfly_4k_High@L5.1_AAC_30M_30fps_3min.ts</a>
+<a href="Netflix/">Netflix/</a>
+<a href="Zapper/">Zapper/</a>
+<a href="apk/">apk/</a>
+<a href="debug/">debug/</a>
+<a href="dvb_video/">dvb_video/</a>
+<a href="http_hls_videos/">http_hls_videos/</a>
+<a href="live555_videos/">live555_videos/</a>
+<a href="qa_test/">qa_test/</a>
+<a href="rtp_udp_videos/">rtp_udp_videos/</a>
+<a href="signature/">signature/</a>
+<a href="signed/">signed/</a>
+<a href="so/">so/</a>
+<a href="software/">software/</a>
+<a href="storage/">storage/</a>
+<a href="tools/">tools/</a>
+<a href="wvcas_video/">wvcas_video/</a>
+</pre>
